b46f9f864a605d06a710638ab654b5589940bf99
DESKTOP-GT4AAMB\stars <apollo.estrella@gmail.com>
2020-04-22 22:30:03 +0800

    Added functionality for allMovies and allAuthors.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,33 +6,53 @@ const resolvers = {
       return await Movies.findAll();
     },
     movie: async (_, args) => {
-      mov = await Movies.findByPk(args.id)
+      mov = await Movies.findByPk(args.id);
       //console.log(mov)
       aut = await Authors.findAll({
         where: {
-          "id": mov['dataValues'].author_id
-        }
-      })
+          id: mov["dataValues"].author_id,
+        },
+      });
       //console.log(aut)
+      movFind = Movie = {
+        id: args.id,
+        name: mov.name,
+        score: mov.score,
+        author: aut[0], // get the first row of array aut
+      };
+      console.log(movFind);
+      return await movFind;
+    },
+    allMovies: async () => {
+      const movies = [];
+      for (const movie of await Movies.findAll()) {
+        mov = await Movies.findByPk(movie.id);
+        //console.log(mov)
+        aut = await Authors.findAll({
+          where: {
+            id: mov["dataValues"].author_id,
+          }
+        });
+        //console.log(aut)
         movFind = Movie = {
-          id: args.id,
+          id: mov.id,
           name: mov.name,
           score: mov.score,
-          author: aut[0]     // get the first row of array aut
+          author: aut[0], // get the first row of array aut
         }
-      console.log(movFind)
-      return await movFind
+        movies.push(movFind)
+      }
+      return movies
     },
     authors: async () => {
       return await Authors.findAll();
     },
     author: async (_, args) => {
       au = await Authors.findByPk(args.id);
-      //console.log(au['dataValues'].id)
       mov = await Movies.findAll({
         where: {
-          "author_id": au['dataValues'].id
-        }
+          author_id: au["dataValues"].id,
+        },
       });
       auFind = Author = {
         id: args.id,
@@ -41,6 +61,28 @@ const resolvers = {
         movies: mov,
       };
       return await auFind;
+    },
+    allAuthors: async () => {
+      const auth = [];
+      var authList = await Authors.findAll();
+      for (const aut of authList) {
+        au = await Authors.findByPk(aut.id);
+        mov = await Movies.findAll({
+          where: {
+            author_id: au["dataValues"].id,
+          },
+        });
+        //console.log(au.id)
+        auFind = Author = {
+          id: au.id,
+          name: au.name,
+          email: au.email,
+          movies: mov,
+        };
+        auth.push(auFind);
+      }
+      console.log(auth);
+      return auth;
     },
   },
   Mutation: {
